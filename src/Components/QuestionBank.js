14a1c25568089a78e5fac0d019eed94124cf1be4
danish <danishrizwan78662@gmail.com>
2024-06-22 16:23:19 +0500

    first commit

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,35 @@
+
+const qBank = [
+	{
+		id: 1,
+		question: "What is the capital of Pakistan?",
+		options: ["Islamabad", "Karachi", "Lahore", "Multan"],
+		answer: "Islamabad",
+	},
+	{
+		id: 2,
+		question: "What is the national game of Pakistan?",
+		options: ["Hockey", "Cricket", "Badminton", "football"],
+		answer: "Hockey",
+	},
+	{
+		id: 3,
+		question: "What is the national animal of Pakistan?",
+		options: ["Goat", "Cow", "Lion", "Markhor"],
+		answer: "Markhor"
+	},
+	{
+		id: 4,
+		question: "What is the born year of Pakistan?",
+		options: ["2003", "1920", "1947", "1948"],
+		answer: "1947"
+	},
+	{
+		id: 5,
+		question: "Which is the biggest province of Pakistan?",
+		options: ["Sindh", "Punjab", "Balochistan", "KPK"],
+		answer: "Balochistan"
+	},
+]
+
+export default qBank;
